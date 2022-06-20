609fcb68b14fc5f758f0bc3585d2cdb81408d607
Pedro Henrique Azarias <ph.azarias@hotmail.com>
2022-06-20 13:41:54 -0300

    Added dollar and euro options in the top select

--- scripts.js
+++ scripts.js
@@ -1,27 +1,139 @@
 const button = document.querySelector('button')
-const realValue = document.getElementById('real-value')
-const currencyValue = document.getElementById('currency-value')
-const dolar = 4.7
-const euro = 5.08
-const select = document.getElementById('currency-select')
-const currencyImage = document.getElementById('currency-img')
-const currencyName = document.getElementById('currency-name')
+
+const dolar = 5.15 //Reais
+const euro = 5.42 //Reais
+
+const euroComparedToDollar = 1.052 //1 Euro é igual à 1,05 Dólar
+const dollarComparedToEuro = 0.9501 // 1 Dólar é igual à 0,95 Euro
+
+const firstSelect = document.getElementById('first-currency-select')
+const secondSelect = document.getElementById('second-currency-select')
+
+const firstCurrencyImage = document.getElementById('first-currency-img')
+const secondCurrencyImage = document.getElementById('second-currency-img')
+
+const firstCurrencyName = document.getElementById('first-currency-name')
+const secondCurrencyName = document.getElementById('second-currency-name')
+
+const firstCurrencyValue = document.getElementById('first-currency-value')
+const secondCurrencyValue = document.getElementById('second-currency-value')
 
 const convertValues = () => {
     const input = document.getElementById('input-real').value
 
-    realValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input)
-    if (select.value === 'US$ Dólar americano') {
-        currencyName.innerHTML = 'Dólar'
-        currencyImage.src = './assets/estados-unidos (1) 1.png'
-        currencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input / dolar)
-    }
-    if (select.value === '€ Euro') {
-        currencyName.innerHTML = 'Euro'
-        currencyImage.src = './assets/euro.png'
-        currencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input / euro)
+    firstCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input)
+        // Se o primeiro for dólar e o segundo for real
+    if (firstSelect.value === 'US$ Dólar americano' && secondSelect.value === 'R$ Real') {
+        firstCurrencyName.innerHTML = 'Dólar'
+        firstCurrencyImage.src = './assets/estados-unidos (1) 1.png'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input)
+
+        secondCurrencyName.innerHTML = 'Real'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input * dolar)
+        secondCurrencyImage.src = './assets/brasil 2.png'
+        secondCurrencyValue
     }
 
+    // Se o primeiro for dólar e o segundo for Euro
+
+    if (firstSelect.value === 'US$ Dólar americano' && secondSelect.value === '€ Euro') {
+        firstCurrencyName.innerHTML = 'Dólar'
+        firstCurrencyImage.src = './assets/estados-unidos (1) 1.png'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input)
+
+        secondCurrencyName.innerHTML = 'Euro'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input / euroComparedToDollar)
+        secondCurrencyImage.src = './assets/euro.png'
+    }
+
+    // Se os dois forem Dólar
+    if (firstSelect.value === 'US$ Dólar americano' && secondSelect.value === 'US$ Dólar americano') {
+        firstCurrencyName.innerHTML = 'Dólar'
+        firstCurrencyImage.src = './assets/estados-unidos (1) 1.png'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input)
+
+        secondCurrencyName.innerHTML = 'Dólar'
+        secondCurrencyImage.src = './assets/estados-unidos (1) 1.png'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input)
+
+    }
+
+    // Se o primeiro for Euro e o segundo for Dólar
+    if (firstSelect.value === '€ Euro' && secondSelect.value === 'US$ Dólar americano') {
+
+        firstCurrencyName.innerHTML = 'Euro'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input)
+        firstCurrencyImage.src = './assets/euro.png'
+
+        secondCurrencyName.innerHTML = 'Dólar'
+        secondCurrencyImage.src = './assets/estados-unidos (1) 1.png'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input / dollarComparedToEuro)
+
+    }
+
+    // Se o primeiro for Euro e o segundo for Real
+    if (firstSelect.value === '€ Euro' && secondSelect.value === 'R$ Real') {
+
+        firstCurrencyName.innerHTML = 'Euro'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input)
+        firstCurrencyImage.src = './assets/euro.png'
+
+        secondCurrencyName.innerHTML = 'Real'
+        secondCurrencyImage.src = 'assets/brasil 2.png'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input * euro)
+    }
+
+    // Se os dois forem Euro
+    if (firstSelect.value === '€ Euro' && secondSelect.value === '€ Euro') {
+
+        firstCurrencyName.innerHTML = 'Euro'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input)
+        firstCurrencyImage.src = './assets/euro.png'
+
+        secondCurrencyName.innerHTML = 'Euro'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input)
+        secondCurrencyImage.src = './assets/euro.png'
+    }
+
+    // Se o primeiro for Real e o segundo for Dólar
+    if (firstSelect.value === 'R$ Real' && secondSelect.value === 'US$ Dólar americano') {
+
+        firstCurrencyName.innerHTML = 'Real'
+        firstCurrencyImage.src = 'assets/brasil 2.png'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input)
+
+
+        secondCurrencyName.innerHTML = 'Dólar'
+        secondCurrencyImage.src = './assets/estados-unidos (1) 1.png'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'USD', minimumFractionDigits: 2 }).format(input / dolar)
+
+    }
+
+    // Se o primeiro for Real e o segundo for Euro
+    if (firstSelect.value === 'R$ Real' && secondSelect.value === '€ Euro') {
+
+        firstCurrencyName.innerHTML = 'Real'
+        firstCurrencyImage.src = 'assets/brasil 2.png'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input)
+
+        secondCurrencyName.innerHTML = 'Euro'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR', minimumFractionDigits: 2 }).format(input / euro)
+        secondCurrencyImage.src = './assets/euro.png'
+
+    }
+
+    // Se os dois forem Real
+    if (firstSelect.value === 'R$ Real' && secondSelect.value === 'R$ Real') {
+
+        firstCurrencyName.innerHTML = 'Real'
+        firstCurrencyImage.src = 'assets/brasil 2.png'
+        firstCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input)
+
+        secondCurrencyName.innerHTML = 'Real'
+        secondCurrencyImage.src = 'assets/brasil 2.png'
+        secondCurrencyValue.innerHTML = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }).format(input)
+
+
+    }
 }
-
 button.addEventListener('click', convertValues)
